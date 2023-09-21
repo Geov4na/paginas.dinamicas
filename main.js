ad27078564ea7662d0e02dc66f2895a700a8d122
Geov4na <112179213+Geov4na@users.noreply.github.com>
2023-09-21 09:03:44 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,20 @@
-// Pom------------
-function tocaSomPom () {
-    document.querySelector('#som_tecla_pom').play();
+function tocaSom (idElementoAudio) {
+
+    document.querySelector(idElementoAudio).play();
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
+
+
+//enquanto
+for ( let contador = 0; contador < listaDeTeclas.length; contador++) {  
+
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`;
+  
+    tecla.onclick = function () {
+        tocaSom(idAudio);
+    }
+ 
+}
